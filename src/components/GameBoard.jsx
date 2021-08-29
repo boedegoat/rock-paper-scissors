0ddd97676a2a8073ba4281ce9ba3226945278335
boedegoat <bhremada.fka@gmail.com>
2021-08-29 13:45:27 +0700

    clean up game board component code

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import Triangle from '../assets/images/bg-triangle.svg'
 import SelectFinger from './SelectFinger'
 
@@ -62,62 +62,63 @@ const GameBoard = () => {
     }
   }, [result])
 
-  return (
-    <Fragment>
-      {userFinger ? (
-        <div className='w-full'>
-          {/* finger display */}
-          <div className='flex justify-between'>
-            <div className='flex flex-col items-center'>
-              <SelectFinger finger={userFinger} />
-              <p className='uppercase text-sm tracking-wide mt-5'>you picked</p>
-            </div>
-            <div className='flex flex-col items-center'>
-              <SelectFinger finger={compFinger} />
-              <p className='uppercase text-sm tracking-wide mt-5'>the house picked</p>
-            </div>
-          </div>
+  if (!userFinger) {
+    return (
+      <div className='relative'>
+        <div className='transform scale-[0.6]'>
+          <img src={Triangle} alt='triangle' />
+        </div>
+        <SelectFinger
+          finger='paper'
+          onClick={() => setUserFinger('paper')}
+          className='cursor-pointer absolute top-0 left-0'
+        />
+        <SelectFinger
+          finger='scissors'
+          onClick={() => setUserFinger('scissors')}
+          className='cursor-pointer absolute top-0 right-0'
+        />
+        <SelectFinger
+          finger='rock'
+          onClick={() => setUserFinger('rock')}
+          className='cursor-pointer absolute bottom-0 left-1/2 transform -translate-x-1/2'
+        />
+      </div>
+    )
+  }
 
-          {/* result display */}
-          <div
-            className={`
+  // if user has selected a finger
+  return (
+    <div className='w-full'>
+      {/* finger display */}
+      <div className='flex justify-between'>
+        <div className='flex flex-col items-center'>
+          <SelectFinger finger={userFinger} />
+          <p className='uppercase text-sm tracking-wide mt-5'>you picked</p>
+        </div>
+        <div className='flex flex-col items-center'>
+          <SelectFinger finger={compFinger} />
+          <p className='uppercase text-sm tracking-wide mt-5'>the house picked</p>
+        </div>
+      </div>
+
+      {/* result display */}
+      <div
+        className={`
           ${result ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}
           mt-10 px-10 text-center`}
-          >
-            <h1 className='uppercase text-5xl'>
-              {result === 'draw' ? 'draw' : `you ${result}`}
-            </h1>
-            <button
-              className='uppercase bg-white text-neutral-dark w-full py-2 tracking-widest mt-4 rounded-md text-sm'
-              onClick={resetGame}
-            >
-              play again
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div className='relative'>
-          <div className='transform scale-[0.6]'>
-            <img src={Triangle} alt='triangle' />
-          </div>
-          <SelectFinger
-            finger='paper'
-            onClick={() => setUserFinger('paper')}
-            className='cursor-pointer absolute top-0 left-0'
-          />
-          <SelectFinger
-            finger='scissors'
-            onClick={() => setUserFinger('scissors')}
-            className='cursor-pointer absolute top-0 right-0'
-          />
-          <SelectFinger
-            finger='rock'
-            onClick={() => setUserFinger('rock')}
-            className='cursor-pointer absolute bottom-0 left-1/2 transform -translate-x-1/2'
-          />
-        </div>
-      )}
-    </Fragment>
+      >
+        <h1 className='uppercase text-5xl'>
+          {result === 'draw' ? 'draw' : `you ${result}`}
+        </h1>
+        <button
+          className='uppercase bg-white text-neutral-dark w-full py-2 tracking-widest mt-4 rounded-md text-sm'
+          onClick={resetGame}
+        >
+          play again
+        </button>
+      </div>
+    </div>
   )
 }
 
